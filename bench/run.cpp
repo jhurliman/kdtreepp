72363d7e691dddf42eaa911b4c627558a030d801
John Hurliman <jhurliman@jhurliman.org>
2021-01-27 03:04:06 -0800

    1.0.0

--- bench/run.cpp
+++ bench/run.cpp
@@ -1,40 +1,86 @@
 #include <benchmark/benchmark.h>
-#include <hello_world.hpp>
 
-static void BM_expensive(benchmark::State& state) // NOLINT google-runtime-references
-{
-    if (state.thread_index == 0)
-    {
-        // Setup code here.
-    }
-    while (state.KeepRunning())
-    {
-        std::string value = hello_world::expensive(static_cast<std::size_t>(state.range(0)));
-        benchmark::DoNotOptimize(value.data());
-        benchmark::ClobberMemory();
-    }
-    if (state.thread_index == 0)
-    {
-        // Teardown code here.
-    }
+#include <Eigen/StdVector>
+#include <kdtreepp.hpp>
+#include <random>
+
+using Vector3 = Eigen::Vector3d;
+using AlignedBox3 = Eigen::AlignedBox3d;
+
+static void BM_Points3dConstruction(benchmark::State& state) {
+  std::vector<Vector3, Eigen::aligned_allocator<Vector3>> points;
+  std::mt19937_64 randGen{size_t(state.thread_index)};
+  std::uniform_real_distribution<double> dist{-1000.0, 1000.0};
+
+  // Make random points
+  const size_t count = size_t(state.range(0));
+  points.resize(count);
+  for (auto& point : points) {
+    point << dist(randGen), dist(randGen), dist(randGen);
+  }
+
+  while (state.KeepRunning()) {
+    const auto node = kdtreepp::MakeEigenKdTreeNode<double, 3>(
+        points.begin(), points.end(), [](const Vector3& p) { return p; },
+        [](const Vector3& p) { return p; });
+    benchmark::DoNotOptimize(node);
+    benchmark::ClobberMemory();
+  }
 }
 
-static void BM_exclaim(benchmark::State& state) // NOLINT google-runtime-references
-{
-    while (state.KeepRunning())
-    {
-        std::string value = hello_world::exclaim("hello");
-        benchmark::DoNotOptimize(value.data());
-        benchmark::ClobberMemory();
+static void BM_Points3dClosest(benchmark::State& state) {
+  std::vector<Vector3, Eigen::aligned_allocator<Vector3>> points;
+  std::mt19937_64 randGen{size_t(state.thread_index)};
+  std::uniform_real_distribution<double> dist{-1000.0, 1000.0};
+
+  // Make random points
+  points.resize(size_t(state.range(0)));
+  for (auto& point : points) {
+    point << dist(randGen), dist(randGen), dist(randGen);
+  }
+
+  const auto node = kdtreepp::MakeEigenKdTreeNode<double, 3>(
+      points.begin(), points.end(), [](const Vector3& p) { return p; },
+      [](const Vector3& p) { return p; });
+
+  const size_t iterations = size_t(state.range(1));
+
+  while (state.KeepRunning()) {
+    for (size_t i = 0; i < iterations; i++) {
+      const Vector3 checkPoint{dist(randGen), dist(randGen), dist(randGen)};
+
+      double minDistSq = std::numeric_limits<double>::max();
+      Vector3 closestPoint;
+      node.visit(
+          [&minDistSq, checkPoint](const AlignedBox3& bounds) {
+            return bounds.squaredExteriorDistance(checkPoint) < minDistSq;
+          },
+          [&minDistSq, &closestPoint, checkPoint](const Vector3& point) {
+            const double rSq = (point - checkPoint).squaredNorm();
+            if (rSq < minDistSq) {
+              minDistSq = rSq;
+              closestPoint = point;
+            }
+          });
+      benchmark::DoNotOptimize(closestPoint);
+      benchmark::ClobberMemory();
     }
+  }
 }
 
-int main(int argc, char* argv[])
-{
-    benchmark::RegisterBenchmark("BM_exclaim", BM_exclaim)->Threads(2)->Threads(4)->Threads(8);                   // NOLINT clang-analyzer-cplusplus.NewDeleteLeaks
-    benchmark::RegisterBenchmark("BM_expensive", BM_expensive)->Threads(2)->Threads(4)->Threads(8)->Arg(1000000); // NOLINT clang-analyzer-cplusplus.NewDeleteLeaks
-    benchmark::Initialize(&argc, argv);
-    benchmark::RunSpecifiedBenchmarks();
+int main(int argc, char* argv[]) {
+  // NOLINTNEXTLINE(clang-analyzer-cplusplus.NewDeleteLeaks)
+  benchmark::RegisterBenchmark("BM_Points3dConstruction", BM_Points3dConstruction)
+      ->Arg(1)
+      ->Arg(10)
+      ->Arg(100)
+      ->Arg(1000)
+      ->Arg(10000)
+      ->Arg(100000)
+      ->Arg(1000000);
+  benchmark::RegisterBenchmark("BM_Points3dClosest", BM_Points3dClosest)->Args({100000, 10000});
+  benchmark::Initialize(&argc, argv);
+  benchmark::RunSpecifiedBenchmarks();
 
-    return 0;
+  return 0;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,21 +1,84 @@
-#include <hello_world.hpp>
+#include <Eigen/StdVector>
+#include <kdtreepp.hpp>
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 
-TEST_CASE("test version")
-{
-    REQUIRE(HELLOWORLD_VERSION_STRING == std::string("1.0.0"));
+using Vector3 = Eigen::Vector3d;
+using AlignedBox3 = Eigen::AlignedBox3d;
+
+static void CHECK_VEC3_EQ(const Vector3& a, const Vector3& b) {
+  CHECK(a[0] == Approx(b[0]));
+  CHECK(a[1] == Approx(b[1]));
+  CHECK(a[2] == Approx(b[2]));
 }
 
-TEST_CASE("test_exclaim")
-{
-    std::string value = hello_world::exclaim("hello");
-    REQUIRE(value == std::string("hello!"));
-}
+TEST_CASE("VERSION_STRING") { REQUIRE(KDTREEPP_VERSION_STRING == std::string("1.0.0")); }
 
-TEST_CASE("test_expensive")
-{
-    std::size_t work = 100;
-    std::string result = hello_world::expensive(work);
-    REQUIRE(result == std::string("Expensive work is finished"));
+TEST_CASE("PointTreeTest") {
+  // Build a k-d tree from a list of points
+
+  std::vector<Vector3, Eigen::aligned_allocator<Vector3>> points;
+  std::mt19937_64 randGen{42};
+  std::uniform_real_distribution<double> dist{-1000.0, 1000.0};
+
+  // Make random points
+  constexpr size_t count = 1500;
+  points.resize(1500);
+  for (auto& point : points) {
+    point << dist(randGen), dist(randGen), dist(randGen);
+  }
+
+  const auto node = kdtreepp::MakeEigenKdTreeNode<double, 3>(
+      points.begin(), points.end(), [](const Vector3& p) { return p; },
+      [](const Vector3& p) { return p; });
+
+  const Vector3 checkPoint{dist(randGen), dist(randGen), dist(randGen)};
+
+  // Find closest point via brute force search
+  double bruteMinDistSq = std::numeric_limits<double>::max();
+  Vector3 bruteClosestPoint;
+  for (const auto& point : points) {
+    const double rSq = (point - checkPoint).squaredNorm();
+    if (rSq < bruteMinDistSq) {
+      bruteMinDistSq = rSq;
+      bruteClosestPoint = point;
+    }
+  }
+
+  // Find closest point using kdtree
+  double minDistSq = std::numeric_limits<double>::max();
+  Vector3 closestPoint;
+  int numBoundsChecks = 0;
+  int numPointChecks = 0;
+  node.visit(
+      [&minDistSq, checkPoint, &numBoundsChecks](const AlignedBox3& bounds) {
+        ++numBoundsChecks;
+        return bounds.squaredExteriorDistance(checkPoint) < minDistSq;
+      },
+
+      [&minDistSq, &closestPoint, checkPoint, &numPointChecks](const Vector3& point) {
+        ++numPointChecks;
+        const double rSq = (point - checkPoint).squaredNorm();
+        if (rSq < minDistSq) {
+          minDistSq = rSq;
+          closestPoint = point;
+        }
+      });
+
+  CHECK_VEC3_EQ(bruteClosestPoint, closestPoint);
+  CHECK(numBoundsChecks < count);
+  CHECK(numPointChecks < count);
+
+  // Count points within a bounding box
+  AlignedBox3 searchBounds{Vector3{-500.0, -500.0, -500.0}, Vector3{500.0, 500.0, 500.0}};
+  size_t foundPoints = 0;
+  node.visit([&searchBounds](const AlignedBox3& bounds) { return searchBounds.intersects(bounds); },
+             [&searchBounds, &foundPoints](const Vector3& point) {
+               if (searchBounds.contains(point)) {
+                 ++foundPoints;
+               }
+             });
+
+  CHECK(foundPoints > count / 10);
+  CHECK(foundPoints < count / 2);
 }
